453120de9947143ba13db2477dde4b04984993f9
Maximiliano Santiago <maximiliano.santiago@gmail.com>
2014-03-17 22:01:10 -0300

    Adding stock brokers listing

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,16 +2,20 @@ package controllers
 
 import play.api.mvc._
 import models.StockBook
+import models.StockBroker
 
 object Application extends Controller {
 
   def index = Action {
-    //Ok(views.html.index("Your new application is ready."))
-    Redirect(routes.Application.stockBooks)
+    Ok(views.html.index())
   }
 
   def stockBooks = Action {
     Ok(views.html.stockbooks.index(StockBook.all()))
+  }
+
+  def stockBrokers = Action {
+    Ok(views.html.stockbrokers.list(StockBroker.all()))
   }
 
 }
--- app/models/StockBroker.scala
+++ app/models/StockBroker.scala
@@ -1,15 +1,29 @@
 package models
 
-import anorm.{NotAssigned, Pk}
+import anorm._
+import anorm.SqlParser._
+import play.api.db._
+import play.api.Play.current
 
 case class StockBroker(
                         id: Pk[Long] = NotAssigned,
                         name: String,
-                        oddLotMarketFee: Double,
-                        defaultLotMarketFee: Double )
+                        oddLotMarketFee: java.math.BigDecimal,
+                        defaultLotMarketFee: java.math.BigDecimal )
 
 object StockBroker {
 
-  def all(): List[StockBroker] = Nil
+  val stockBroker = {
+    get[Pk[Long]]("id") ~
+      get[String]("name") ~
+      get[java.math.BigDecimal]("odd_lot_market_fee") ~
+      get[java.math.BigDecimal]("default_lot_market_fee")  map {
+      case id~name~oddLotMarketFee~defaultLotMarketFee => StockBroker(id, name, oddLotMarketFee, defaultLotMarketFee)
+    }
+  }
+
+  def all(): List[StockBroker] = DB.withConnection { implicit c =>
+    SQL("select * from stock_broker").as(stockBroker *)
+  }
 
 }
